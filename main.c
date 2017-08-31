0af8bd4786832e86e0f34274bff89e180e3e1644
Linh <linhvtd00569@fpt.edu.vn>
2017-08-31 21:20:50 +0700

    Thêm nữa này

--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 main() {
 	printf("%i \n", plus(132,145));
-	printf("%i", minus(185,465));
+	printf("%i \n", minus(32,465));
+	printf("%i \n", multiply(142,365));
+	printf("%i \n", devide(465,125));
 	return 0;
 }
 //cong hai so voi tham so au vao là a, b là hai so nguyen
@@ -11,3 +13,10 @@ int plus(int a, int b){
 int minus(int a,int b){
 	return a-b;
 }
+int multiply(int a,int b){
+	return a*b;
+}
+int devide(int a,int b){
+	return a/b;
+}
+
